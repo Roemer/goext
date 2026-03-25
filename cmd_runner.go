2b16f37932edfb48cdd2cc29c5373488c098d4c1
Roemer <r.baeriswyl@gmail.com>
2026-03-25 16:22:59 +0100

    feat: Added logFilePath to runner

--- cmd_runner.go
+++ cmd_runner.go
@@ -27,6 +27,7 @@ type CmdRunner struct {
 	OutputToConsole       bool
 	SkipPostProcessOutput bool
 	AdditionalEnv         map[string]string
+	LogFilePath           string
 }
 
 // Creates a new CmdRunner with the given options.
@@ -40,29 +41,54 @@ func NewCmdRunner() *CmdRunner {
 // Runs the command with the given options.
 func (r *CmdRunner) Run(executable string, arguments ...string) error {
 	cmd := r.asCmd(executable, arguments...)
+
+	var stdoutWriters, stderrWriters []io.Writer
 	if r.OutputToConsole {
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
+		stdoutWriters = append(stdoutWriters, os.Stdout)
+		stderrWriters = append(stderrWriters, os.Stderr)
 	}
-	err := cmd.Start()
-	if err != nil {
+	if r.LogFilePath != "" {
+		logFile, err := os.OpenFile(r.LogFilePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.ModePerm)
+		if err != nil {
+			return err
+		}
+		defer logFile.Close()
+		stdoutWriters = append(stdoutWriters, logFile)
+		stderrWriters = append(stderrWriters, logFile)
+	}
+	cmd.Stdout = io.MultiWriter(stdoutWriters...)
+	cmd.Stderr = io.MultiWriter(stderrWriters...)
+
+	if err := cmd.Start(); err != nil {
 		return err
 	}
-	err = cmd.Wait()
-	return err
+	return cmd.Wait()
 }
 
 // Runs the command and returns the separate output from stdout and stderr.
 func (r *CmdRunner) RunGetOutput(executable string, arguments ...string) (string, string, error) {
 	cmd := r.asCmd(executable, arguments...)
+
 	var stdoutBuf, stderrBuf bytes.Buffer
+	var stdoutWriters, stderrWriters []io.Writer
 	if r.OutputToConsole {
-		cmd.Stdout = io.MultiWriter(os.Stdout, &stdoutBuf)
-		cmd.Stderr = io.MultiWriter(os.Stderr, &stderrBuf)
-	} else {
-		cmd.Stdout = &stdoutBuf
-		cmd.Stderr = &stderrBuf
+		stdoutWriters = append(stdoutWriters, os.Stdout)
+		stderrWriters = append(stderrWriters, os.Stderr)
 	}
+	if r.LogFilePath != "" {
+		logFile, err := os.OpenFile(r.LogFilePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.ModePerm)
+		if err != nil {
+			return "", "", err
+		}
+		defer logFile.Close()
+		stdoutWriters = append(stdoutWriters, logFile)
+		stderrWriters = append(stderrWriters, logFile)
+	}
+	stdoutWriters = append(stdoutWriters, &stdoutBuf)
+	stderrWriters = append(stderrWriters, &stderrBuf)
+	cmd.Stdout = io.MultiWriter(stdoutWriters...)
+	cmd.Stderr = io.MultiWriter(stderrWriters...)
+
 	err := cmd.Run()
 	if r.SkipPostProcessOutput {
 		return stdoutBuf.String(), stderrBuf.String(), err
@@ -73,14 +99,25 @@ func (r *CmdRunner) RunGetOutput(executable string, arguments ...string) (string
 // Runs the command and returns the output from stdout and stderr combined.
 func (r *CmdRunner) RunGetCombinedOutput(executable string, arguments ...string) (string, error) {
 	cmd := r.asCmd(executable, arguments...)
+
 	var outBuf bytes.Buffer
+	var writers []io.Writer
 	if r.OutputToConsole {
-		cmd.Stdout = io.MultiWriter(os.Stdout, &outBuf)
-		cmd.Stderr = io.MultiWriter(os.Stderr, &outBuf)
-	} else {
-		cmd.Stdout = &outBuf
-		cmd.Stderr = &outBuf
+		writers = append(writers, os.Stdout)
 	}
+	if r.LogFilePath != "" {
+		logFile, err := os.OpenFile(r.LogFilePath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.ModePerm)
+		if err != nil {
+			return "", err
+		}
+		defer logFile.Close()
+		writers = append(writers, logFile)
+	}
+	writers = append(writers, &outBuf)
+	combined := io.MultiWriter(writers...)
+	cmd.Stdout = combined
+	cmd.Stderr = combined
+
 	err := cmd.Run()
 	if r.SkipPostProcessOutput {
 		return outBuf.String(), err
@@ -131,12 +168,20 @@ func (r *CmdRunner) WithEnvs(envs map[string]string) *CmdRunner {
 	return clone
 }
 
+// Sets a file path to which all command output (stdout + stderr) is written.
+func (r *CmdRunner) WithLogFile(filePath string) *CmdRunner {
+	clone := r.Clone()
+	clone.LogFilePath = filePath
+	return clone
+}
+
 // Clones the CmdRunner with its current configuration.
 func (r *CmdRunner) Clone() *CmdRunner {
 	clone := NewCmdRunner()
 	clone.WorkingDirectory = r.WorkingDirectory
 	clone.OutputToConsole = r.OutputToConsole
 	clone.SkipPostProcessOutput = r.SkipPostProcessOutput
+	clone.LogFilePath = r.LogFilePath
 	clone.AdditionalEnv = make(map[string]string)
 	maps.Copy(clone.AdditionalEnv, r.AdditionalEnv)
 	return clone
@@ -152,7 +197,7 @@ func (r *CmdRunner) asCmd(executable string, arguments ...string) *exec.Cmd {
 	if r.WorkingDirectory != "" {
 		cmd.Dir = r.WorkingDirectory
 	}
-	// Set the additonal environment variables
+	// Set the additional environment variables
 	if len(r.AdditionalEnv) > 0 {
 		// Make sure to add the current environment variables first
 		cmd.Env = os.Environ()
